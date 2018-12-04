1a38af04402560ceeb2c6dfc63158d00f335366b
Alexey Chuprin <a.chuprin@capjack.ru>
2018-12-05 00:43:38 +0300

    Gradle 5.0, nebula.release 9.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 	`java-gradle-plugin`
 	`maven-publish`
 	id("com.gradle.plugin-publish") version "0.10.0"
-	id("nebula.release") version "8.0.3"
+	id("nebula.release") version "9.0.0"
 	id("io.freefair.sources-jar") version "2.7.3"
 }
 
